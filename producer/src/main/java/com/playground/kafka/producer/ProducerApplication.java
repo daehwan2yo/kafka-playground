469c7cfbcbd6abc6e4e070d5aff7d1043dddfcda
mac_daehwan2yo <daehwan7989@naver.com>
2022-08-07 16:27:36 +0900

    producer, consumer init

---
+++ producer/src/main/java/com/playground/kafka/producer/ProducerApplication.java
@@ -0,0 +1,16 @@
+package com.playground.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author : daehwan2yo
+ * Date : 2022/08/07
+ * Info : 
+ **/
+@SpringBootApplication
+public class ProducerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+}
